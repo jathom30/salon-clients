8defc7ae01e94dc2ba6feb94c798e3422a3847fd
Jeff Thomas <jathom30@gmail.com>
2023-10-10 10:55:06 -0400

    sick model bro (#1)

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,8 @@
 
 import type { LoaderArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
+import Spline from '@splinetool/react-spline';
+import { Suspense } from "react";
 import { Chair } from "~/assets";
 import { FlexList, Link } from "~/components";
 import { getUserId } from "~/session.server";
@@ -16,9 +18,11 @@ export async function loader({ request }: LoaderArgs) {
 
 export default function Index() {
   return (
-    <main className="h-full flex items-center justify-center flex-col gap-4">
-      <div className="w-full max-w-xs max-h-96 relative">
-        <Chair />
+    <main className="h-full flex items-center justify-center flex-col gap-4 overflow-hidden">
+      <div className="w-full h-full max-w-xs max-h-96 relative">
+        <Suspense fallback={<Chair />}>
+          <Spline scene="https://prod.spline.design/w8qwq3kvieiBwXxA/scene.splinecode" />
+        </Suspense>
       </div>
       <h1 className="text-5xl font-bold text-primary">Salon Clients</h1>
       <FlexList direction="row">

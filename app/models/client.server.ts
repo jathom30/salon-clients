525bcedf05e7d9324db9c56e44b6c9202c04a44b
Jeff Thomas <jathom30@gmail.com>
2024-07-24 16:32:54 -0400

    add admin route

--- app/models/client.server.ts
+++ app/models/client.server.ts
@@ -1,39 +1,63 @@
-import type { User, Client } from '@prisma/client'
+import type { User, Client } from "@prisma/client";
 
 import { prisma } from "~/db.server";
-import type { ExpectedFileType } from '~/routes/clients/user/upload';
+import type { ExpectedFileType } from "~/routes/clients/user/upload";
 
-export function getClient({ id, userId }: Pick<Client, 'id'> & { userId: User['id'] }) {
+export function getClient({
+  id,
+  userId,
+}: Pick<Client, "id"> & { userId: User["id"] }) {
   return prisma.client.findFirst({
-    where: { id, userId }
-  })
+    where: { id, userId },
+  });
 }
 
-export function getClients({ userId, q }: { userId: User['id']; q?: string }) {
+export function getClients({ userId, q }: { userId: User["id"]; q?: string }) {
   return prisma.client.findMany({
     where: {
       userId,
       ...(q && {
         name: {
-          contains: q
-        }
-      })
+          contains: q,
+        },
+      }),
     },
     select: { id: true, name: true },
-    orderBy: { name: 'asc' }
-  })
+    orderBy: { name: "asc" },
+  });
 }
 
-export async function getClientsAndNotes(userId: User['id']) {
+export function getAllClients() {
+  return prisma.client.findMany({
+    include: {
+      user: true,
+    },
+    orderBy: { name: "asc" },
+  });
+}
+
+export async function getClientsAndNotes(userId: User["id"]) {
   return prisma.client.findMany({
     where: { userId },
-    select: { name: true, email: true, phoneNumber: true, note: { select: { createdAt: true, body: true } } },
-  })
+    select: {
+      name: true,
+      email: true,
+      phoneNumber: true,
+      note: { select: { createdAt: true, body: true } },
+    },
+  });
 }
 
 export function createClient({
-  name, email, phoneNumber, userId
-}: { name: Client['name'], email?: Client['email'], phoneNumber: Client['phoneNumber'] } & { userId: User['id'] }) {
+  name,
+  email,
+  phoneNumber,
+  userId,
+}: {
+  name: Client["name"];
+  email?: Client["email"];
+  phoneNumber: Client["phoneNumber"];
+} & { userId: User["id"] }) {
   return prisma.client.create({
     data: {
       name,
@@ -41,38 +65,49 @@ export function createClient({
       phoneNumber,
       user: {
         connect: {
-          id: userId
-        }
-      }
-    }
-  })
+          id: userId,
+        },
+      },
+    },
+  });
 }
 
-export function deleteClient({ id, userId }: Pick<Client, 'id'> & { userId: User['id'] }) {
+export function deleteClient({
+  id,
+  userId,
+}: Pick<Client, "id"> & { userId: User["id"] }) {
   return prisma.client.deleteMany({
-    where: { id, userId }
-  })
+    where: { id, userId },
+  });
 }
 
-export function updateClient(id: Client['id'], client: Partial<Client>) {
+export function updateClient(id: Client["id"], client: Partial<Client>) {
   return prisma.client.update({
     where: { id },
-    data: client
-  })
+    data: client,
+  });
 }
 
-export async function bulkUpload(clients: ExpectedFileType, userId: Client['userId']) {
-  return await Promise.all(clients.map(async client => {
-    return await prisma.client.create({
-      data: {
-        name: client.name,
-        email: client.email,
-        phoneNumber: client.phoneNumber,
-        userId,
-        note: {
-          create: client.notes.map(note => ({ body: note.body, createdAt: new Date(note.createdAt) }))
-        }
-      }
+export async function bulkUpload(
+  clients: ExpectedFileType,
+  userId: Client["userId"]
+) {
+  return await Promise.all(
+    clients.map(async (client) => {
+      return await prisma.client.create({
+        data: {
+          name: client.name,
+          email: client.email,
+          phoneNumber: client.phoneNumber,
+          userId,
+          note: {
+            create: client.notes.map((note) => ({
+              body: note.body,
+              createdAt: new Date(note.createdAt),
+            })),
+          },
+        },
+      });
     })
-  }))
+  );
 }
--- app/routes/clients.tsx
+++ app/routes/clients.tsx
@@ -1,27 +1,49 @@
-import { faCog, faPlus, faSignOut, faUser } from "@fortawesome/free-solid-svg-icons";
-import { Outlet, Link as RemixLink, Form, useLoaderData, useNavigation } from "@remix-run/react";
+import {
+  faCog,
+  faPlus,
+  faSignOut,
+  faUser,
+  faUserTie,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  Outlet,
+  Link as RemixLink,
+  Form,
+  useLoaderData,
+  useNavigation,
+} from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useState } from "react";
 import { Chair } from "~/assets";
-import { Button, FlexHeader, FlexList, Link, Loader, MaxWidth, MobileModal, Navbar, Title } from "~/components";
+import {
+  Button,
+  FlexHeader,
+  FlexList,
+  Link,
+  Loader,
+  MaxWidth,
+  MobileModal,
+  Navbar,
+  Title,
+} from "~/components";
 import { requireUser } from "~/session.server";
-import { useSpinDelay } from 'spin-delay';
+import { useSpinDelay } from "spin-delay";
 
 export async function loader({ request }: LoaderArgs) {
-  const user = await requireUser(request)
-  return json({ user })
+  const user = await requireUser(request);
+  return json({ user });
 }
 
 export default function Clients() {
-  const { user } = useLoaderData<typeof loader>()
-  const [showMenu, setShowMenu] = useState(false)
-  const navigation = useNavigation()
-  const isLoading = useSpinDelay(navigation.state !== 'idle')
+  const { user } = useLoaderData<typeof loader>();
+  const [showMenu, setShowMenu] = useState(false);
+  const navigation = useNavigation();
+  const isLoading = useSpinDelay(navigation.state !== "idle");
 
   return (
     <main className="mt-16 h-full">
-      <div className="fixed top-0 inset-x-0 z-20">
+      <div className="fixed inset-x-0 top-0 z-20">
         <Navbar>
           <FlexHeader>
             <RemixLink to=".">
@@ -39,8 +61,23 @@ export default function Clients() {
               </FlexList>
             </RemixLink>
             <FlexList direction="row" items="center">
-              <Link to="new" kind="primary" icon={faPlus} isCollapsing>New client</Link>
-              <Button onClick={() => setShowMenu(true)} size="md" isCollapsing icon={faUser} kind="ghost">User</Button>
+              {user.email === "jathom30@gmail.com" ? (
+                <Link to="admin" kind="secondary" icon={faUserTie} isCollapsing>
+                  Admin
+                </Link>
+              ) : null}
+              <Link to="new" kind="primary" icon={faPlus} isCollapsing>
+                New client
+              </Link>
+              <Button
+                onClick={() => setShowMenu(true)}
+                size="md"
+                isCollapsing
+                icon={faUser}
+                kind="ghost"
+              >
+                User
+              </Button>
             </FlexList>
           </FlexHeader>
         </Navbar>
@@ -50,14 +87,24 @@ export default function Clients() {
       </MaxWidth>
       <MobileModal open={showMenu} onClose={() => setShowMenu(false)}>
         <FlexList pad={4}>
-          <Link to="user" icon={faCog} onClick={() => setShowMenu(false)} isOutline size="md">User settings</Link>
+          <Link
+            to="user"
+            icon={faCog}
+            onClick={() => setShowMenu(false)}
+            isOutline
+            size="md"
+          >
+            User settings
+          </Link>
           <Form method="post" action="/logout">
             <FlexList>
-              <Button type="submit" icon={faSignOut}>Log out</Button>
+              <Button type="submit" icon={faSignOut}>
+                Log out
+              </Button>
             </FlexList>
           </Form>
         </FlexList>
       </MobileModal>
     </main>
-  )
+  );
 }

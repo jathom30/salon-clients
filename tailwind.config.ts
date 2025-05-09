d2274b35d2f89ac2d9c9a0d7516271bf236c3b63
Jeff Thomas <jathom30@gmail.com>
2025-05-09 14:37:35 -0400

    revert daisy update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,26 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  safelist: ["gap-4"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      borderWidth: {
+        1: "1px",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["winter", "dracula"],
+    styled: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+  },
 } satisfies Config;
